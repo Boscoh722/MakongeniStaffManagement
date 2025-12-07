2c00cf277e24366779d55627d75fe6cba74c29f4
Boscoh722 <boscobrilli8@gmail.com>
2025-12-07 11:54:50 +0300

    MVP

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base URL from environment or fallback
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://staffmanagement-p9eq.onrender.com',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:10000/api',
 });
 
 // Add token automatically to all requests
